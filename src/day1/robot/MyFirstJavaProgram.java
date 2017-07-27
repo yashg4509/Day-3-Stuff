bc0a2f630a7ea6523c8a0d75d7e4250d477c1c38
yashg4509 <bludgermoonstone66@gmail.com>
2017-07-26 22:26:39 -0700

    Add files via upload

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -6,7 +6,19 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
-		// START HERE
+		Robot R2D2 = new Robot();
+		R2D2.sparkle(); //Sparkles while moving
+		R2D2.penDown(); //Draws line
+		
+		for(int i = 0; i<10; i++) {  //Runs code multiple times (i.e 10 times) 
+		R2D2.move(300); //Moves the robot
+		R2D2.turn(270);//Turns the bot
+		R2D2.setPenColor(Color.red);
+		}
+		
+		
+		
+		
 
 	
 		
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,19 +6,24 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot R2D2 = new Robot();
 		// 5. Set your robot's pen to the down position
-
+		R2D2.penDown();
 		// 3. Set the robot to go at max speed (10)
-
+			R2D2.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
-
+			for (int i = 0; i < 75; i++) {
+				R2D2.turn(360/7);
+				R2D2.move(5 * i);
+				R2D2.setRandomPenColor();
+				R2D2.setPenWidth(i);
+			}
 			// 7. Change the pen color to random
 	
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 	
 			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
+			
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
 	
 			// 9. Set the pen width to i
